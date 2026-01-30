811abdc4d30e5aa7ccadd4a4d51c1e86d9dd30af
Gian Paolo Ouano <gianpaol@e5-012-w14.ece.ualberta.ca>
2026-01-30 11:09:12 -0700

    Lab 2

--- src/roots/roots.cpp
+++ src/roots/roots.cpp
@@ -1,27 +1,129 @@
 #include "roots.hpp"
+#include <cmath>
 
 bool bisection(std::function<double(double)> f,
                double a, double b,
                double *root) {
+    const double tolerance = 1e-6;
+    const int Max_Iter = 1e6;
+    double fa = f(a);
+    double fb = f(b);
+    if(fa == 0.0){
+        *root = a;
+        return true;
+    }
+    if(fb == 0.0){
+        *root = b;
+        return true;
+    }
+    if (fa * fb > 0) {
+        return false;
+    }
+
+
+    for (int i = 0; i < Max_Iter; i++){
+        double c = (a+b)*0.5;
+        double fc = f(c);
+
+        if(std::fabs(fc) <= tolerance || std::fabs(b-a) <= tolerance) {
+            *root = c;
+            return true;
+        }
+        if(fa*fc < 0) {
+            b = c;
+            fb = fc;
+        }
+        else {
+            a = c;
+            fa = fc;
+        };
+    }
     return false;
+    };
+
+
+bool regula_falsi(std::function<double(double)> f, double a, double b, double *root){
+    const double tolerance = 1e-6;
+    const int Max_Iter = 1e6;
+
+    double fa = f(a);
+    double fb = f(b);
+    if (fa * fb > 0) {
+        return false; 
+    }
+    for (int i = 0; i < Max_Iter; ++i) {
+        double c = a - ((f(a)*(b-a))/(fb - fa)); // formula given
+        double fc = f(c);
+        if (std::fabs(fc) < tolerance) { 
+            *root = c; 
+            return true;
+        }
+        if (fa * fc < 0) { 
+            b = c;
+            fb = fc;
+        } else {
+            a = c;
+            fa = fc;
+        }
+    }
+    return false; 
 }
 
-bool regula_falsi(std::function<double(double)> f,
-                  double a, double b,
-                  double *root) {
-    return false;
+
+
+bool newton_raphson(std::function<double(double)> f, std::function<double(double)> g, double a, double b, double c, double *root){
+    const double tolerance = 1e-6;
+    const int Max_Iter = 1e6;
+    
+   
+
+    double x_n = c; // set initial guess
+    if (f(a) * f(b) > 0){ 
+        return false;
+    }
+    for(int i = 0; i < Max_Iter; i++){
+        double fx_n =f(x_n);
+        double gx_n = g(x_n);
+        if (std::fabs(gx_n) < tolerance) { // check derivative isnt too small/zero (to avoid division by zero error)
+            return false;
+        }
+
+        double x_new = x_n - (fx_n/gx_n); 
+        double fx = f(x_new);
+        if(std::fabs(fx) < tolerance){ 
+            *root = x_new;
+            return true;
+        }
+
+        x_n = x_new; 
+    }
+    return false; 
 }
 
-bool newton_raphson(std::function<double(double)> f,
-                    std::function<double(double)> g,
-                    double a, double b, double c,
-                    double *root) {
-    return false;
-}
+bool secant(std::function <double(double)> f, double a, double b, double c, double *root){ 
+    const double tolerance = 1e-6;
+    const int Max_Iter = 1e6;
+    
+    double x_old = c+0.1; // first initial guess
+    double x_current = c; // second initial guess
 
-bool secant(std::function<double(double)> f,
-            double a, double b, double c,
-            double *root) {
-    return false;
+    if(f(a) * f(b) > 0){
+        return false;
+    }
+    for(int i = 0; i < Max_Iter; i++){
+        if (std::fabs((f(x_current) - f(x_old))) < tolerance){ // avoid division by zero
+            return false; 
+        }
+        double x_new = x_current - f(x_current)*((x_current - x_old)/(f(x_current) - f(x_old)));
+        if (std::fabs(f(x_new)) < tolerance){
+            *root = x_new;
+            return true;
+        }
+        else {
+             x_old = x_current;
+            x_current = x_new;
+        }
+    }
+    return false; 
 }
 
